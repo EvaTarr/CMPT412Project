25c8bcd7bc14cb2a676a7c2ebfcec9487c15706c
evatarr <elt783@usask.ca>
2023-11-15 10:16:26 -0600

    Remove data folder -data is too big to store -Added memePosts.py

--- README.txt
+++ README.txt
@@ -1,5 +1,10 @@
 Project
+** data files are too big to store on github (free version)  **
 
 truckerPosts.py
     -sentiment vs likes
     -the-2022-trucker-strike-on-reddit-comments.csv
+memePosts.py
+    -comments and posts (not sure what exactly will be compared yet, looking into it)
+    -one-year-of-doge-on-reddit-posts.csv
+    -one-year-of-doge-on-reddit-comments.csv
--- truckerPosts.py
+++ truckerPosts.py
@@ -3,36 +3,37 @@
 # Checking to see if there is a correlation to post popularity (# of likes) and the sentiment connected to it
 import pandas as pd
 import matplotlib.pyplot as plt
-
-
 def truckerPosts():
     file_path = "the-2022-trucker-strike-on-reddit-comments.csv"
-    allTruckerPosts = pd.read_csv(file_path, encoding='utf-8')
+    # number of posts to get (top n)
     n = 50
+    # read file with utf-8
+    allTruckerPosts = pd.read_csv(file_path, encoding='utf-8')
+    # drop all empty columns
     truckerPosts_cleaned = allTruckerPosts.dropna(axis=1, how='all')
     truckerPosts_sorted = truckerPosts_cleaned.sort_values(by='score', ascending=False)
     top_posts = truckerPosts_sorted.head(n)
 
     fig, leftAxis = plt.subplots(figsize=(20, 10))
 
-    leftAxis.bar(top_posts['id'], top_posts['score'], color='skyblue', label='Score')
-    leftAxis.set_xlabel('Post Title')
-    leftAxis.set_ylabel('Score', color='skyblue')
-    leftAxis.tick_params('y', colors='skyblue')
+    leftAxis.bar(top_posts['id'], top_posts['score'], color='lightsteelblue', label='Score')
+    leftAxis.set_xlabel('Post ID')
+    leftAxis.set_ylabel('Score', color='lightsteelblue')
+    leftAxis.tick_params('y', colors='lightsteelblue')
+    # rotate all the x ID's to be vertical for better readability
+    plt.xticks(rotation='vertical')
 
+    # create a right axis to display the sentiment values
     rightAxis = leftAxis.twinx()
-    rightAxis.plot(top_posts['id'], top_posts['sentiment'], color='orange', marker='o', label='Sentiment')
-    rightAxis.set_ylabel('sentiment', color='orange')
-    rightAxis.tick_params('y', colors='orange')
+    rightAxis.plot(top_posts['id'], top_posts['sentiment'], color='red', marker='o', label='Sentiment')
+    rightAxis.set_ylabel('sentiment', color='red')
+    rightAxis.tick_params('y', colors='red')
 
     plt.title(f'Top {n} Most Liked Posts with Sentiment')
     fig.tight_layout()
-    fig.legend(loc='upper left', bbox_to_anchor=(0.8, 0.8))
-
-    # Rotate x-axis labels for better readability
-    plt.xticks(rotation=90, ha='right')
-
+    # place the legend in the top right hand side
+    fig.legend(loc='upper right', bbox_to_anchor=(0.95, 0.95))
     plt.show()
 
-#Run
+# run
 truckerPosts()
